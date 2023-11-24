019127f9d26db1e4c8998a2987b2a2ad0bff3857
zefuo <mr.calculus7@gmail.com>
2023-11-24 21:50:56 +0300

    first commit

---
+++ src/components/Board.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+    </>
+  );
+}
+export default Board;
